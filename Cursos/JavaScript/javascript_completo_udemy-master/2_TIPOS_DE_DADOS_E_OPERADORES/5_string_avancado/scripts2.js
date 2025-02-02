6c4e37f11f44d2efa35ce771215640888beceb7f
Ciro Guilherme Nass <ciroguilhermenass@gmail.com>
2025-02-01 22:42:23 -0300

    secao 1 e 2 concluidas

---
+++ Cursos/JavaScript/javascript_completo_udemy-master/2_TIPOS_DE_DADOS_E_OPERADORES/5_string_avancado/scripts2.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha \nSegunda Linha")
+console.log("O meu nome é 'Ciro'")
+console.log('O meu nome é "Ciro"')
+console.log(`A multiplicação de 5 por 3 é ${5*3}`)
+console.log("O " + "meu " +"nome" +" é " + "Ciro")
---
+++ Cursos/JavaScript/javascript_completo_udemy-master/2_TIPOS_DE_DADOS_E_OPERADORES/6_boolean/scripts2.js
@@ -0,0 +1,5 @@
+console.log(typeof true)
+console.log(false)
+console.log(5 > 2)
+console.log(3 > 10)
+console.log(typeof(3 > 10))
