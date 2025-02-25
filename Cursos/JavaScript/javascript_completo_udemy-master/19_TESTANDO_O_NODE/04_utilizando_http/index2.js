10f075ab60a2bb707d0b88b54dd97e14db6099be
Ciro Guilherme Nass <ciroguilhermenass@gmail.com>
2025-02-25 07:47:28 -0300

    conclusao secao 19

---
+++ Cursos/JavaScript/javascript_completo_udemy-master/19_TESTANDO_O_NODE/04_utilizando_http/index2.js
@@ -0,0 +1,14 @@
+const {createServer} = require('https');
+
+let server = createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(`
+        <h1>Hello World!</h1>
+        <p>Primeira página com Node.js</p>
+    `);
+    response.end();
+});
+
+server.listen(8000);
+
+console.log("Ouvindo a Porta 8000");
